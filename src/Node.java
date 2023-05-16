d919c46615f84959f0c184c5d31aea9319c08a14
Kirsten Ali <alik5@cardiff.ac.uk>
2023-05-16 21:09:54 +0100

    binarySearch

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+public class Node{
+    Node left;
+    Node right;
+    int val;
+
+    public Node(Node left, Node right, int val) {
+        this.left = left;
+        this.right = right;
+        this.val = val;
+    }
+}
