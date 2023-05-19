4b79ac1eeca73f5397d24cd47ed2a893c3a592e8
Kirsten Ali <alik5@cardiff.ac.uk>
2023-05-19 14:03:45 +0100

    LengthOfLastWordv2

--- src/LengthOfLastWordv2.java
+++ src/LengthOfLastWordv2.java
@@ -11,7 +11,7 @@ public class LengthOfLastWordv2 {
                     count++;
                 }
             }
-
+            
             else{
                 if(s.charAt(i) ==' ')
                     break;
