28e36d614218f0b55d59145a92db005116bbe69f
Kirsten Ali <alik5@cardiff.ac.uk>
2023-05-19 13:58:22 +0100

    LengthOfLastWordv2

---
+++ src/LengthOfLastWord.java
@@ -0,0 +1,9 @@
+// runtime can be improved
+public class LengthOfLastWord {
+
+    public int lengthOfLastWord(String s) {
+        var words = s.split(" ");
+
+        return words[words.length-1].length();
+    }
+}
