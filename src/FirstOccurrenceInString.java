d346259acef794c127f01452dd0f367b0a9276bb
Kirsten Ali <ikirstenhd@gmail.com>
2023-05-27 19:43:44 +0100

    FirstOccurrenceInString

---
+++ src/FirstOccurrenceInString.java
@@ -0,0 +1,37 @@
+public class FirstOccurrenceInString {
+    // time worst case, n-m(m), where n is the length of haystack and m is the length of needle
+    public int strStr(String haystack, String needle) {
+
+
+        if(needle.length() > haystack.length())
+            return -1;
+
+        else{
+            var lengthDiff = haystack.length() - needle.length();
+
+            for(int i=0; i<=lengthDiff; i++)
+            {
+                var count =i;
+                for(int j=0; j<needle.length(); j++){
+
+                    if(needle.charAt(j) != haystack.charAt(count))
+                        break;
+
+                    else if(j==needle.length()-1)
+                        return i;
+
+                    count++;
+
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String [] args){
+        var result = new FirstOccurrenceInString().strStr("leetcode", "leeto");
+
+        System.out.println(result);
+    }
+}
