dfe489d640d2bcb4f897786e0eca1dea8a6d25f9
Kirsten Ali <ikirstenhd@gmail.com>
2023-05-28 17:39:10 +0100

    ClimbingStairs

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,23 @@
+// this problem can be translated to the fibonacci sequence. We can solve this easily with recursion.
+// time complexity is bad. each climbStairs call (excluding base cases)executes exactly two further climbStairs, thus time is 2^n
+// consider iterative approach or dynamic programming to improve complexity
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int result;
+
+        if(n ==1)
+            return 1;
+
+        else if(n == 2)
+            return 2;
+
+        else if(n==3)
+            return 3;
+
+        else {
+            result = climbStairs(n-1) + climbStairs(n-2);
+
+            return result;
+        }
+    }
+}
