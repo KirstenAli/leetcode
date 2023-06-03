2f7dc9d6745eb3e4ae1f0b6679c79dca8dabef74
Kirsten Ali <ikirstenhd@gmail.com>
2023-06-03 19:48:06 +0100

    Conditions added as comment

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,3 +1,13 @@
+/*
+An input string is valid if:
+
+1. Open brackets must be closed by the same type of brackets.
+2. Every close bracket has a corresponding open bracket of the same type.
+
+Every close bracket has a corresponding open bracket of the same type.
+
+ */
+
 public class ValidParentheses {
     public boolean isValid(String s) {
         var openBracketFound = false;
@@ -11,7 +21,6 @@ public class ValidParentheses {
         var openBracketCount = 0;
         var openCurlyCount = 0;
         var openSquareCount = 0;
-
 
         for (int i=0; i<s.length();i++)
         {
