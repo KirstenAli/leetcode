a68cfe9c91c3b371debbc909ae239c6632ce8e3b
Kirsten Ali <ikirstenhd@gmail.com>
2023-06-05 22:00:17 +0100

    code simplified.

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -6,58 +6,35 @@ public class MergeTwoSortedLists {
         var head = new ListNode();
         var mergedList = head;
 
-        while(true){
-            if(list1 ==null && list2==null){
-                break;
-            }
+        while(list1.next !=null && list2.next !=null){
 
-            else if(list1 ==null){
-                appendList(list2, mergedList);
-                break;
-            }
-
-            else if(list2==null){
-                appendList(list1, mergedList);
-                break;
-            }
-
-            else if(list1.val < list2.val){
+            if(list1.val <= list2.val){
                 var node = new ListNode(list1.val);
                 mergedList.next = node;
                 mergedList = node;
                 list1 = list1.next;
-
             }
 
-            else if(list1.val > list2.val){
+            else{
                 var node = new ListNode(list2.val);
                 mergedList.next = node;
                 mergedList = node;
                 list2 = list2.next;
-
-            }
-
-            else{
-                var node1 = new ListNode(list1.val);
-                var node2 = new ListNode(list1.val);
-
-                mergedList.next=node1;
-                mergedList.next.next=node2;
-                mergedList = node2;
-
-                list1 = list1.next;
-                list2 = list2.next;
             }
 
         }
+
+        if(list1.next!=null) appendList(list2, mergedList);
+
+        else if(list2.next!=null) appendList(list1, mergedList);
 
         return head.next;
     }
 
 
     // time o(1)
-    public ListNode appendList(ListNode list1, ListNode list2){
-        return list2.next=list1;
+    public void appendList(ListNode list1, ListNode list2){
+        list2.next = list1;
     }
 
     public static void main(String[] args){
