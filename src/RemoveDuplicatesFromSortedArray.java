c1229397315e6ebfc0daa42998ffdb8178a0d7d6
Kirsten Ali <alik5@cardiff.ac.uk>
2023-06-10 00:08:25 +0100

    RemoveDuplicatesFromSortedArray

---
+++ src/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        var map = new HashMap<Integer, Integer>();
+        Integer[] expectedNums;
+
+        for (int i=0; i<nums.length; i++)
+            map.put(nums[i], nums[i]);
+
+        expectedNums = map.values().toArray(new Integer[0]);
+        nums =  Arrays.stream(expectedNums).mapToInt(i->i).toArray();
+
+        return expectedNums.length;
+    }
+}
