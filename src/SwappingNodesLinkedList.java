dd7fee70f7228669c8ab52e8818e73d0ca9dde87
Kirsten Ali <alik5@cardiff.ac.uk>
2023-06-10 18:55:14 +0100

    code refactored.

--- src/SwappingNodesLinkedList.java
+++ src/SwappingNodesLinkedList.java
@@ -1,92 +1,42 @@
-
-// more challenging: instead of simply swapping the node values, swap the nodes instead:
-
-class ListWrapper{
-    ListNode pre;
-    ListNode target;
-
-    ListNode next;
-
-    public ListWrapper(ListNode pre,
-                       ListNode target,
-                       ListNode next) {
-        this.pre = pre;
-        this.target = target;
-        this.next = next;
-    }
-}
-
 public class SwappingNodesLinkedList {
 
+
     public ListNode swapNodes(ListNode head, int k) {
-        var length = linkListLength(head);
+        var listLength = LinkListLength.linkListLength(head);
+        var secondPos = (listLength-k)+1;
+        var node = head;
+        ListNode firstNode = head;
+        ListNode secondNode=head.next;
 
-        var firstWrapper = getWrapper(head, k);
-        var firstPre = firstWrapper.pre;
-        var firstTarget = firstWrapper.target;
-        var firstNext = firstWrapper.next;
+        var count = Math.max(secondPos, k);
 
-        var secondPos = (length - k) + 1;
-        var secondWrapper = getWrapper(head, secondPos);
-        var secondPre = secondWrapper.pre;
-        var secondTarget = secondWrapper.target;
-        var secondNext = secondWrapper.next;
-
-        if (secondPos - k == -1) {
-            secondPre.next = firstTarget;
-            var temp = firstTarget.next;
-            firstTarget.next = secondTarget;
-            secondTarget.next = temp;
-        }
-        else {
-
-            if (secondPos - k == 1) {
-                secondTarget.next = firstTarget;
-            }
-            else {
-
-                secondPre.next = firstTarget;
-                secondTarget.next = firstNext;
+        for (int i = 1; i <= count; i++) {
+            if (i == k) {
+                firstNode = node;
             }
 
-            firstPre.next = secondTarget;
-            firstTarget.next = secondNext;
+            if (i == secondPos) {
+                secondNode = node;
+            }
+
+            node = node.next;
         }
 
-        if(k==1)
-            head = secondTarget;
 
-        else if(k==length)
-            head = firstTarget;
+        var temp = firstNode.val;
+
+        firstNode.val = secondNode.val;
+        secondNode.val = temp;
 
         return head;
     }
 
-    public int linkListLength(ListNode node){
-        var head = node;
+    public static void main(String[] args){
+        var intArray = new int[]{100, 90};
 
-        var count =1;
+        var list = ArrayToListNode.arrayToListNode(intArray);
 
-        while(head.next!=null) {
-            head = head.next;
-            count++;
-        }
+        new SwappingNodesLinkedList().swapNodes(list, 2);
 
-        return count;
     }
-
-    public ListWrapper getWrapper(ListNode node, int targetPos){
-        var count =1;
-        var head = node;
-        var preListNode= new ListNode();
-
-        while(count !=targetPos){
-            preListNode = head;
-            head = head.next;
-            count++;
-        }
-
-        return new ListWrapper(preListNode, head, head.next);
-    }
-
 }
