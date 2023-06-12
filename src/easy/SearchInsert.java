34825800e670272fe9a811092af6fcc71066c013
Kirsten Ali <ikirstenhd@gmail.com>
2023-06-12 19:56:14 +0100

    code refactored

--- src/easy/SearchInsert.java
+++ src/easy/SearchInsert.java
@@ -3,22 +3,15 @@ package easy;
 // we divide by two each time, time complexity = log2(n)
 public class SearchInsert {
     public int searchInsert(int[] nums, int target) {
-        var length = nums.length-1;
-        var start = 0;
-        var end =length;
-        int midPoint;
 
-        while (true) {
-            var range = end-start;
-            midPoint = start+range/2;
+        var start = 0;
+        var end =nums.length-1;
+        int midPoint = 0;
+
+        while (start<=end) {
+            midPoint = start+end/2;
 
             if (nums[midPoint] == target) {
-                break;
-            }
-
-            else if(range==0){
-                if(nums[midPoint]<target)
-                    midPoint++;
                 break;
             }
 
@@ -32,13 +25,16 @@ public class SearchInsert {
 
         }
 
+        if(nums[midPoint]<target)
+            midPoint++;
+
         return midPoint;
     }
 
     public static void main(String[] args){
         var intArray = new int[]{1, 3, 5, 6, 8, 10, 20};
 
-        var result = new SearchInsert().searchInsert(intArray, 2);
+        var result = new SearchInsert().searchInsert(intArray, 6);
 
         System.out.println(result);
     }
