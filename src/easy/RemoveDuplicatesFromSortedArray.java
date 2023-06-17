91e8e40d614415b21f95f393bfc95414c7d1efa5
Kirsten Ali <ikirstenhd@gmail.com>
2023-06-17 22:35:21 +0100

    time and space complexity calculated

--- src/easy/RemoveDuplicatesFromSortedArray.java
+++ src/easy/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,5 @@
 package easy;
-// with this approach we are not using a hashset to store the numbers to the space complexity in reduced to constant
+// with this approach we are not using a hashset to store the numbers so the space complexity in reduced to constant
 // time is in n, where n is the amount of nums.
 
 public class RemoveDuplicatesFromSortedArray {
